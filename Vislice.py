5c827e8dc1f4be94cbdbc33e1337bb8c75cef716
Simon Grad <simon.grad3@gmail.com>
2021-05-11 15:55:31 +0200

    Dodali cookie

--- Vislice.py
+++ Vislice.py
@@ -1,7 +1,7 @@
 import bottle
 import Modul
 
-vislice = Modul.Vislice()
+COOKIE_SECRET = "KJNFOR90OKEIJ03POJR02JRO3'oif3j20k'2e3e20"
 
 @bottle.get("/")
 def index():
@@ -18,10 +18,16 @@ def nova_igra():
 
     vislice.zapisi_v_datoteko(Modul.DATOTEKA_ZA_SHRANJEVANJE)
 
-    bottle.redirect(novi_url)
+    bottle.response.set_cookie("ID_IGRE", str(id_igre), path="/", secret=COOKIE_SECRET)
 
-@bottle.get("/igra/<id_igre:int>/")
-def pokazi_igro(id_igre):
+    bottle.redirect("/igraj/")
+
+
+@bottle.get("/igraj/")
+def pokazi_igro():
+
+    id_igre = int(bottle.request.get_cookie("ID_IGRE", secret=COOKIE_SECRET))
+
     vislice = Modul.Vislice.preberi_iz_datoteke(
         Modul.DATOTEKA_ZA_SHRANJEVANJE
     )
@@ -29,15 +35,18 @@ def pokazi_igro(id_igre):
     vislice.zapisi_v_datoteko(Modul.DATOTEKA_ZA_SHRANJEVANJE)
     return bottle.template("igra.tpl", igra=trenutna_igra, stanje=trenutno_stanje)
 
-@bottle.post("/igra/<id_igre:int>/")
-def ugibaj_na_igri(id_igre):
+
+@bottle.post("/igraj/")
+def ugibaj_na_igri():
+    id_igre = int(bottle.request.get_cookie("ID_IGRE", secret=COOKIE_SECRET))
+
     vislice = Modul.Vislice.preberi_iz_datoteke(
         Modul.DATOTEKA_ZA_SHRANJEVANJE
     )
     ugibana = bottle.request.forms["crka"]
     vislice.ugibaj(id_igre, ugibana) 
     vislice.zapisi_v_datoteko(Modul.DATOTEKA_ZA_SHRANJEVANJE)
-    bottle.redirect(f"/igra/{id_igre}/")
+    bottle.redirect(f"/igraj/")
     
 @bottle.route("/img/<file_path:path>")
 def img_static(file_path):
